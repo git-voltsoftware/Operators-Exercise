1facab748d7389d07f7d8ea3bc7097c2cb08f05f
git-volt.software <info@volt.software>
2022-12-01 19:59:12 -0500

    added operators

--- OperatorExercise/Program.cs
+++ OperatorExercise/Program.cs
@@ -3,8 +3,23 @@
     public class Program
     {
         static void Main(string[] args)
-        {
-            Console.WriteLine("Hello, World!");
+        {   //Exercise 1
+            //create integer variables
+            int a = 17;
+            int b = 4;
+            
+            int quotient = a / b;
+            int remainder = a % b;
+
+            if ( a == 17 && b == 4){
+                Console.WriteLine($" 17/4 {quotient} is {b} remainder is {remainder}.");
+            }
+
+
+
+
+
+
         }
     }
 }
